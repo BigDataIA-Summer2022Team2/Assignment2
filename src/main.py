9e3cf850b4b93c2b93d094e3069101777ec6caeb
Meihu Qin <56311544+1207Lemon@users.noreply.github.com>
2022-06-24 20:25:42 -0400

    Implemented logging for API calls

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI, Query, Path
+import random
+import string
+from fastapi import FastAPI, Query, Path, Request
 from typing import Union
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from log.logger import logger
 import time
 from pydantic import BaseModel
 from pathlib import Path
 import sys
+import logging
+import logging.config
+from requests import request
 
 path = str(Path(Path(__file__).parent.absolute()))
 sys.path.insert(0, path)
@@ -22,6 +26,13 @@ from api_functions import displayImage
 from api_functions import returnHomePage
 from api_functions import displaymodelcardhtmloutput
 
+# setup loggers
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+
+# get root logger
+logger = logging.getLogger(__name__)  # the __name__ resolve to "main" since we are at the root of the project. 
+                                      # This will get the root logger since no logger in the configuration has this name.
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -29,18 +40,23 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Home page
 @app.get("/", response_class=HTMLResponse)
 async def home():
-    logger.info('=============== API start ===============')
-    logger.info('mode: HTTP GET')
-    funcName = getattr(returnHomePage.getHomePage,'__name__')
-    logger.info('Call: {interface}',interface=funcName)
-    logger.info( 'Now time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
     response = returnHomePage.getHomePage()
-    logger.info('=============== API end ===============\n\n')
+    return response
 
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    idem = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    logger.info(f"rid={idem} start request path={request.url.path}")
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = (time.time() - start_time) * 1000
+    formatted_process_time = '{0:.2f}'.format(process_time)
+    logger.info(f"rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code}")
+    
     return response
 
 # info 8 values for filter and return response
-@app.get("/api/get/infoFilter/") # 127.0.0.1/api/get/infoFilter/
+@app.get("/api/get/infoFilter/")
 async def inputInfoFilterRequest(filename:Union[str,None]= Query(default="", max_length=40),
                               width:Union[int,None] = Query(default=0),
                               height:Union[int,None] = Query(default=0),
@@ -49,64 +65,29 @@ async def inputInfoFilterRequest(filename:Union[str,None]= Query(default="", max
                               ymin:Union[int,None] = Query(default=0),
                               xmax:Union[int,None] = Query(default=0),
                               ymax:Union[int,None] = Query(default=0)):
-    logger.info('=============== API start ===============')
-    logger.info('mode: HTTP GET')
-    logger.info( 'Function start time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    funcName = getattr(getS3BucketBody.getS3BucketBodyInfo,'__name__') # return function name which is called
-    logger.info('Call: {interface}',interface=funcName)
-    
-    className = className.upper() #v22 V22
+    className = className.upper()
+
     response = getS3BucketBody.getS3BucketBodyInfo(filename,width,height,className,xmin,ymin,xmax,ymax) # get return response
-    
-    if(response == {}):
-        logger.error("No data Found: HTTP 404")
-    
-    
-    logger.info( 'Function end time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    logger.info('=============== API end ===============\n\n')
     return  response
 
 # fileName , class input and return response
 @app.get("/api/get/fileNameAndClass/")
 async def aircraftClassAndFileNameRequest(className:str,
                               filename:Union[str,None]= Query(default="", max_length=32)):
-    logger.info('=============== API start ===============')
-    logger.info('mode: HTTP GET')
-    logger.info('Function start time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    funcName = getattr(fileNameAndClassNameFiltered.getFileNameClassNameFilteredResult,'__name__') # return function name which is called
-    logger.info('Call: {interface}',interface=funcName)
     className = className.upper()
     
     response = fileNameAndClassNameFiltered.getFileNameClassNameFilteredResult(className,filename) # get return response
-    
-    if(response == {}):
-        logger.error("No data Found: HTTP 404")
         
-    logger.info( 'Function end time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    logger.info('=============== API end ===============\n\n')
-    
-    
-    
     return  response
 
 # (width , weight) range and get response
 @app.get("/api/get/imgSizeRange/")
 async def imgSzieRangeRequest(width:Union[int,None] = Query(default=0),
                               height:Union[int,None] = Query(default=0)):
-     
-    logger.info('=============== API start ===============')
-    logger.info('mode: HTTP GET')
-    logger.info('Function start time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    funcName = getattr(imgSizeRangeFiltered.getimgSizeRangeFilteredResult,'__name__') # return function name which is called
-    logger.info('Call: {interface}',interface=funcName)
     
     response = imgSizeRangeFiltered.getimgSizeRangeFilteredResult(width,height) # get return response
     
-    if(response == {}):
-        logger.error("No data Found: HTTP 404")
     
-    logger.info( 'Function end time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    logger.info('=============== API end ===============\n\n')
     return  response
 
 # (xmin,ymin,xmax,ymax) range and get response
@@ -116,37 +97,18 @@ async def aircraftPositionRequest(xmin:Union[int,None] = Query(default=0),
                                   xmax:Union[int,None] = Query(default=0),
                                   ymax:Union[int,None] = Query(default=0)):
     
-    logger.info('=============== API start ===============')
-    logger.info('mode: HTTP GET')
-    logger.info('Function start time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    funcName = getattr(aircraftPositionFilter.getAircraftPositionFilterResult,'__name__') # return function name which is called
-    logger.info('Call: {interface}',interface=funcName)
-    
     response = aircraftPositionFilter.getAircraftPositionFilterResult(xmin,ymin,xmax,ymax) # get return response
-    
-    if(response == {}):
-        logger.error("No data Found: HTTP 404")
-    
-    logger.info( 'Function end time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    logger.info('=============== API end ===============\n\n')
+
     return  response
 
 # No input value and get response(all info)
 @app.get('/api/get/allInfo/')
 async def getAllImgInfo():
-    logger.info('=============== API start ===============')
-    logger.info('mode: HTTP GET')
-    logger.info('Function start time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    funcName = getattr(getS3BucketBody.getS3BucketBodyInfo,'__name__') # return function name which is called
-    logger.info('Call: {interface}',interface=funcName)
+
     
     response = getS3BucketBody.getS3BucketBodyInfo() # get return response
     
-    if(response == {}):
-        logger.error("No data Found: HTTP 404")
     
-    logger.info( 'Function end time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    logger.info('=============== API end ===============\n\n')
     return response
 
 # input aircraft Num and output all images == numbers
@@ -154,41 +116,21 @@ async def getAllImgInfo():
 @app.get("/api/get/aircraftNumandClass/")
 async def numAndClassFiteredInfoRequest(num:int,
                                          className:Union[str,None] = Query(default="")):
-    logger.info('=============== API start ===============')
-    logger.info('mode: HTTP GET')
-    logger.info('Function start time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    
-    funcName = getattr(numAndClassNameFiltered.getNumAndClassFilteredResult,'__name__') # return function name which is called
-    logger.info('Call: {interface}',interface=funcName)
     
     className = className.upper()
     response = numAndClassNameFiltered.getNumAndClassFilteredResult(num,className)
     
-    
-    if(response == {}):
-        logger.error("No data Found: HTTP 404")
         
-    logger.info( 'Function end time: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    logger.info('=============== API end ===============\n\n')
     return  response
 
 
 # get random Num images info
 @app.get("/api/get/random/{num}")
 async def getNumRandomImage(num: int = Path(title="Number of random aircrafts", gt=0, le=9)): 
-    
-    logger.info('=============== API start ===============')
-    funcName = getattr(getNumRandomImages.getNumRandomImageFileNames,'__name__')
-    logger.info('Call: {interface}',interface=funcName)
-    logger.info( 'Process start: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
     response =  getNumRandomImages.getNumRandomImageFileNames(num)
     
-    if(response == {}):
-        logger.error("No data Found: HTTP 404")
         
-    logger.info( 'Process end: {tiems}',tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    logger.info('=============== API end ===============')
     return response
 
 ######################### Featured API for display #########################
@@ -197,18 +139,9 @@ async def getNumRandomImage(num: int = Path(title="Number of random aircrafts", 
 @app.get("/pandas/html/csv/", response_class=HTMLResponse)
 async def getPandasCsvOutputHtmlPage():
 
-    logger.info('=============== API start ===============')
-
-    funcName = getattr(displayPandasCsvHtmlOutput.getPandasProfilingCsvHtmlOutput,'__name__')
-    logger.info('Interface Name: {interface}\tNow Time: {tiems}',interface=funcName, tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-
 
     response = displayPandasCsvHtmlOutput.getPandasProfilingCsvHtmlOutput()
 
-    if(response == {}):
-        logger.error("No data Found: HTTP 404")
-
-    logger.info('=============== API end ===============\n\n')
 
     return response
 
@@ -216,50 +149,23 @@ async def getPandasCsvOutputHtmlPage():
 # display pandas profling img info html output
 @app.get("/pandas/html/image/", response_class=HTMLResponse)
 async def getPandasImageOutputHtmlPage():
-    logger.info('=============== API start ===============')
-
-    funcName = getattr(displayPandasImagesHtmlOutput.getPandasProfilingImageHtmlOutput,'__name__')
-    logger.info('Interface Name: {interface}\tNow Time: {tiems}',interface=funcName, tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
 
     response = displayPandasImagesHtmlOutput.getPandasProfilingImageHtmlOutput()
-
-    if(response == {}):
-        logger.error("No data Found: HTTP 404")
-
-    logger.info('=============== API end ===============\n\n')
 
     return response
     
 @app.get("/modelcard/html/", response_class=HTMLResponse)
 async def getModelCardOutputHtmlPage():
 
-    logger.info('=============== API start ===============')
-
-    funcName = getattr(displaymodelcardhtmloutput.displayModelCardHtmlOutput,'__name__')
-    logger.info('Interface Name: {interface}\tNow Time: {tiems}',interface=funcName, tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-
-
     response = displaymodelcardhtmloutput.displayModelCardHtmlOutput()
-
-    if(response == {}):
-        logger.error("No data Found: HTTP 404")
-
-    logger.info('=============== API end ===============\n\n')
 
     return response
 
-#Todo 
-# Bug
+#Todo
 # display random image and its info
 @app.get("/display/image/", response_class=HTMLResponse)
 async def displayImageInHTML():
-    logger.info('=============== API start ===============')
-
-    funcName = getattr(displayImage.showRandomImg,'__name__')
-    logger.info('Interface Name: {interface}\tNow Time: {tiems}',interface=funcName, tiems=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-
-    logger.info('=============== API end ===============\n\n')
 
     return displayImage.showRandomImg()
 
