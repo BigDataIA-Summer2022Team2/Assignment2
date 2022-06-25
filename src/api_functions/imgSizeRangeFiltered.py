2d0e278d058a63040adf3e3ae9a372f6788bf1ad
Meihu Qin <56311544+1207Lemon@users.noreply.github.com>
2022-06-24 23:55:04 -0400

    Update imgSizeRangeFiltered.py

--- src/api_functions/imgSizeRangeFiltered.py
+++ src/api_functions/imgSizeRangeFiltered.py
@@ -10,14 +10,15 @@ import boto3
     Give us the maximum value of the image size, and we will find every image which is in that range.
     """
 def getimgSizeRangeFilteredResult(width=0,height=0):
-    try:
-        """
+    """
         It takes in a width and height, and returns a dictionary of all the images in the S3 bucket that are
         smaller than the width and height
         
         :param width: The minimum width of the image, defaults to 0 (optional)
         :param height: The height of the image you want to filter by, defaults to 0 (optional)
-        """
+    """
+    try:
+        
         key = 'csv/combined.csv'
         abs_path = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))
         csv_path = abs_path+"/credentials/aws_s3_credentials.json"
@@ -40,7 +41,6 @@ def getimgSizeRangeFilteredResult(width=0,height=0):
         inner_index_num = csv_header_value_list[0].split(',')   # header 0-7 [0] file_name
         index_no = 0
         result={}
-
 
 
         for i in range(len(csv_header_value_list)):
